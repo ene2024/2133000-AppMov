256b7f6b6a26ab2289611645b2cf7c0fe9233a80
Carlos Garcia <carloseduardogarher@gmail.com>
2024-03-09 18:20:41 -0600

    4 Directiva: ngClass

--- miAppMovil-2133000/src/app/home/home.page.ts
+++ miAppMovil-2133000/src/app/home/home.page.ts
@@ -15,5 +15,7 @@ export class HomePage {
   //Variables que son Usadas para ngFor
   variableC: any = ["Articulo 1", "Articulo 2", "Articulo 3","Articulo 4","Articulo 5"];
   variableD: any = ["$10", "$20", "$30","$40","50"];
-  
+
+  //Variable que son Usadas por [ngClass]
+  variableE: boolean[] = [true,true,false,false,true];
 }
