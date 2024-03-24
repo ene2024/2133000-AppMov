45ebca26bb17a63f44ff46e0c613da04059055eb
Carlos Garcia <carloseduardogarher@gmail.com>
2024-03-24 01:27:27 -0600

    Ciclo de Vida de un Componente

--- miAppMovil-2133000/src/app/home/home.page.ts
+++ miAppMovil-2133000/src/app/home/home.page.ts
@@ -7,7 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    console.log("AppComponent: Constructor")
+  }
+ngOnChanges(){
+  console.log("AppComponent: OnChanges")
+}
+ngOnInit(){
+  console.log("AppComponent: OnInit")
+}
+ngDoCheck(){
+  console.log("AppComponent: DoCheck")
+}
+ngAfterContentInit(){
+  console.log("AppComponent: AfterContentInit")
+}
+ngAfterContentChecked(){
+  console.log("AppComponent: AfterContentChecked")
+}
+ngAfterViewInit(){
+  console.log("AppComponent: AfterViewInit")
+}
+ngAfterViewChecked(){
+  console.log("AppComponent: AfterViewChecked")
+}
+ngOnDestroy(){
+  console.log("AppComponent: OnDestroy")
+}
   //Variables que son Usadas para ngIF
   variableA: boolean=true;
   variableB: boolean=false;
