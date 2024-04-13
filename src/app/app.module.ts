ebe147faf456dcce0e1b0c9ee4a03c16a5c478cd
Carlos Garcia <carloseduardogarher@gmail.com>
2024-04-12 21:18:12 -0600

    TareasApp Visual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { TareasComponent } from './tareas/tareas.component';
+import { AgregarTareaComponent } from './agregar-tarea/agregar-tarea.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,TareasComponent,AgregarTareaComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
